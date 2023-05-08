f92b024ce0157323a4a9365639a83de4fc46961f
JSR5404 <joshrice5404@gmail.com>
2023-05-08 03:51:29 -0400

    index code

--- index.js
+++ index.js
@@ -1,3 +1,61 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
+const {Triangle, Circle, Square} = require('./lib/shapes.js');
+
+const questions = [
+    {
+      type: "input",
+        message: "Enter Three Logo Letters",
+        name: "letters",
+      validate: (input) => {
+        if (input.length > 3) {return false} else {return true}
+      },
+    },
+    {
+      type: "list",
+      message: "Select Your Shape",
+      name: "shapes",
+      choices: ["Square", "Triangle", "Circle"],
+    },
+    {
+      type: "input",
+      message: "Select Your Color",
+      name: "color",
+    },
+  ];
+
+  function init() {
+    inquirer.prompt(questions).then((response) => {generateSVG(response.color, response.shapes, response.letters)})};
+
+    function generateSVG(color, shapes, letters) {
+        let newShape;
+        let code;
+        switch (shapes) {
+                
+                case 'Square':
+                newShape = new Square();
+                newShape.setColor(color);
+                newShape.setText(letters);
+                code = newShape.render();
+                break
+                
+                case 'Triangle':
+                newShape = new Triangle();
+                newShape.setColor(color);
+                newShape.setText(letters);
+                code = newShape.render();
+                break
+                
+                case 'Circle':
+                newShape = new Circle();
+                newShape.setColor(color);
+                newShape.setText(letters);
+                code = newShape.render();
+                break
+            
+        }
+console.log("New Logo Created!");
+        fs.writeFileSync(path.join(__dirname, '.', 'examples', 'yourlogo.svg'), code)
+    };
+    init();
